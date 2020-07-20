ea8e93dde80d2b780bec767ce5d954ed9b75ef4e
karoly15 <karolyi15@hotmail.com>
2020-07-19 23:02:01 -0600

    Title Scene Update

--- src/Controllers/Views/TitleScene_Controller.java
+++ src/Controllers/Views/TitleScene_Controller.java
@@ -3,6 +3,9 @@ package Controllers.Views;
 import Controllers.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class TitleScene_Controller {
 
@@ -12,8 +15,16 @@ public class TitleScene_Controller {
     private Button start_Button;
     @FXML
     private Button about_Button;
+    @FXML
+    private ImageView logo;
+    @FXML
+    private AnchorPane scene;
 
-
+    @FXML
+    private void initialize(){
+        scene.setStyle("-fx-background-color: #000000");
+        this.logo.setImage(new Image("file:Resources/Imgs/UI/Title.jpg"));
+    }
     public void setMainApp(Main mainApp){
         this.mainApp=mainApp;
     }
